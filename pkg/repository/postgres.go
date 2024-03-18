c9613ca6bc04d66c7a08c84b8d6806fd36aabb81
nikita <368050@edu.itmo.ru>
2024-03-18 16:00:31 +0300

    delete comment postgres.go

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -24,8 +24,7 @@ type Config struct {
 func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
-	/*db, err := sqlx.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
-	cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode))*/
+	
 	if err != nil {
 		return nil, err
 	}
